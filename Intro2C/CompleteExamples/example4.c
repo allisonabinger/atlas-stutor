fd718e65f1f0a290e2d9bc30c4357c7ef3f82ecf
Allison <allisonabinger@gmail.com>
2024-01-12 14:36:45 -0600

    A - 01/12 02:36 PM - spaces >> tabs

--- Intro2C/CompleteExamples/example4.c
+++ Intro2C/CompleteExamples/example4.c
@@ -7,14 +7,14 @@
 */
 int main(void)
 {
-  int num = 0;
-  int limit = 10;
+	int num = 0;
+	int limit = 10;
 
-  while (num < limit)
-  {
-    printf("The number is %d, which is less than the limit: %d\n", num, limit);
-    num++;
-    sleep(1);
-  }
-  printf("You have reached the limit of %d!\n\n", limit);
+	while (num < limit)
+	{
+		printf("The number is %d, which is less than the limit: %d\n", num, limit);
+		num++;
+		sleep(1);
+	}
+	printf("You have reached the limit of %d!\n\n", limit);
 }
