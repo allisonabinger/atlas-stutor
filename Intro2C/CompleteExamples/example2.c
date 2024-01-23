0c4be01fed8f54071cc054092275cf96822116b1
Allison Binger <127708538+allisonabinger@users.noreply.github.com>
2024-01-23 13:25:39 -0600

    added comments to example2 + return statement

--- Intro2C/CompleteExamples/example2.c
+++ Intro2C/CompleteExamples/example2.c
@@ -7,18 +7,24 @@
 int main(void)
 {
 	int num = 5;
+	/* variable declaration */
 
 	if (num < 3)
+	/* conditional statement */
 	{
 		printf("The number %d is less than 3\n", num);
+		/* print using %d format specifier */
 	}
 	else if (num > 6)
 	{
 		printf("The number %d is greater than 6\n", num);
+		/* print using %d format specifier */
 	}
 	else
 	{
 		printf("The number %d is between 3 and 6\n", num);
+		/* print using %d format specifier */
 	}
+	return (0);
 }
 
