b32d148d959cd0708505d7de4ab79e48b98754ff
Allison Binger <127708538+allisonabinger@users.noreply.github.com>
2024-01-23 13:29:59 -0600

    added comments and return statement

--- Intro2C/CompleteExamples/example4.c
+++ Intro2C/CompleteExamples/example4.c
@@ -9,12 +9,15 @@ int main(void)
 {
 	int num = 0;
 	int limit = 10;
+	/* delcares and assigns values to variables num and limit */
 
 	while (num < limit)
+	/* complete this loop until the condition is met */
 	{
-		printf("The number is %d, which is less than the limit: %d\n", num, limit);
-		num++;
-		sleep(1);
+		printf("The number is %d, which is less than the limit: %d\n", num, limit); /* prints */
+		num++; /* increments num */
+		sleep(1); /* waits one second for loop */
 	}
-	printf("You have reached the limit of %d!\n\n", limit);
+	printf("You have reached the limit of %d!\n\n", limit); /* prints once outside of loop */
+	return (0); /* returns int value to indiciate success */
 }
